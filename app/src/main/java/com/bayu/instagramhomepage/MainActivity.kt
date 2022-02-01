968946036c19e61abbdccc843eb54e6ea3507e60
bayu07750 <bayu07750@gmail.com>
2022-02-01 20:16:16 +0700

    initial commit

---
+++ app/src/main/java/com/bayu/instagramhomepage/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.bayu.instagramhomepage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.bayu.instagramhomepage.ui.theme.InstagramHomePageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstagramHomePageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    InstagramHomePageTheme {
+        Greeting("Android")
+    }
+}
