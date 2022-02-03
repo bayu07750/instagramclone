5b155102037182cb7d4405c5e29061a971b4be58
bayu07750 <bayu07750@gmail.com>
2022-02-03 22:05:12 +0700

    create PostFooter

--- app/src/main/java/com/bayu/instagramhomepage/ui/home/HomeScreen.kt
+++ app/src/main/java/com/bayu/instagramhomepage/ui/home/HomeScreen.kt
@@ -1,21 +1,22 @@
 package com.bayu.instagramhomepage.ui.home
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AddBox
-import androidx.compose.material.icons.outlined.ChatBubbleOutline
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -23,9 +24,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bayu.instagramhomepage.R
 import com.bayu.instagramhomepage.ui.MainViewModel
@@ -74,7 +80,7 @@ fun HomeContent(
         item {
             Divider(modifier = Modifier.padding(top = 12.dp))
         }
-        items(15) {
+        items(20) {
             Post(
                 onShowBottomSheet = onShowBottomSheet,
             )
@@ -169,8 +175,111 @@ fun PostContent(
 }
 
 @Composable
-fun PostFooter() {
-    // TODO not implement yet
+fun ColumnScope.PostFooter() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Row {
+            Icon(
+                imageVector = Icons.Outlined.FavoriteBorder,
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Icon(
+                imageVector = Icons.Outlined.ChatBubbleOutline,
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Icon(
+                imageVector = Icons.Outlined.Send,
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+        Icon(
+            imageVector = Icons.Outlined.BookmarkBorder,
+            contentDescription = null,
+            modifier = Modifier.size(28.dp)
+        )
+    }
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .animateContentSize(
+                animationSpec = tween(600),
+            ),
+    ) {
+        var isShowMore by remember { mutableStateOf(false) }
+
+        Text(text = "1,432 likes", fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
+        Spacer(modifier = Modifier.height(4.dp))
+        Row {
+            val description = buildAnnotatedString {
+                withStyle(
+                    SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp
+                    )
+                ) {
+                    append("samehadaku.care")
+                }
+                append(" ")
+                append("is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+            }
+            if (isShowMore) {
+                Column(
+                    modifier = Modifier.weight(1F),
+                ) {
+                    Text(
+                        text = description,
+                        fontSize = 14.sp,
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                        Text(
+                            text = "less",
+                            modifier = Modifier
+                                .clickable { isShowMore = false },
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            } else {
+                Text(
+                    text = description,
+                    modifier = Modifier.weight(1F),
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = "more",
+                        modifier = Modifier
+                            .clickable { isShowMore = true },
+                        fontSize = 14.sp
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = "View All 53 comments",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W500,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "2 days aog", fontSize = 12.sp)
+        }
+    }
 }
 
 @Composable
