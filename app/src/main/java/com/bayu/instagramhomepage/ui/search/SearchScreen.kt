743350f84db073c8fc5483477ed7be89e1cbad13
bayu07750 <bayu07750@gmail.com>
2022-02-11 07:54:48 +0700

    create SearchBar

--- app/src/main/java/com/bayu/instagramhomepage/ui/search/SearchScreen.kt
+++ app/src/main/java/com/bayu/instagramhomepage/ui/search/SearchScreen.kt
@@ -1,16 +1,55 @@
 package com.bayu.instagramhomepage.ui.search
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@Preview(showBackground = true)
 @Composable
 fun SearchScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Search".uppercase(), style = MaterialTheme.typography.h3)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        SearchBar()
+    }
+}
+
+@Composable
+fun SearchBar() {
+    Surface(
+        modifier = Modifier
+            .padding(horizontal = 12.dp)
+            .padding(top = 16.dp, bottom = 8.dp),
+        shape = RoundedCornerShape(8.dp),
+        color = Color(0xFFe2e8f0),
+        contentColor = Color.Black,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                contentDescription = "Search Icon"
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "Search",
+                color = Color(0xFF94a3b8)
+            )
+        }
     }
 }
