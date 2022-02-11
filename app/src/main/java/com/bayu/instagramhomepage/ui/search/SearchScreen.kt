9f94d6d631456d93f9969fcf0800ec475abe74df
bayu07750 <bayu07750@gmail.com>
2022-02-11 09:20:28 +0700

    create search content

--- app/src/main/java/com/bayu/instagramhomepage/ui/search/SearchScreen.kt
+++ app/src/main/java/com/bayu/instagramhomepage/ui/search/SearchScreen.kt
@@ -1,6 +1,9 @@
 package com.bayu.instagramhomepage.ui.search
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
@@ -11,9 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bayu.instagramhomepage.R
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview(showBackground = true)
 @Composable
 fun SearchScreen() {
@@ -22,6 +29,46 @@ fun SearchScreen() {
             .fillMaxSize()
     ) {
         SearchBar()
+        SearchContent()
+    }
+}
+
+@Composable
+fun ColumnScope.SearchContent() {
+    LazyColumn {
+        items(10) {
+            Row(
+                modifier = Modifier.weight(3F)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .weight(1F)
+                        .heightIn(min = 120.dp, max = 120.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(2.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .weight(1F)
+                        .heightIn(min = 120.dp, max = 120.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(2.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .weight(1F)
+                        .heightIn(min = 120.dp, max = 120.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(2.dp))
+        }
     }
 }
 
