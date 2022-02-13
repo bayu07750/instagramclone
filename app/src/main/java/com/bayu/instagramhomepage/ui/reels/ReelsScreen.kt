277bdd50b8f558c03c828fcde37bed456efc285b
bayu07750 <bayu07750@gmail.com>
2022-02-13 13:46:42 +0700

    separate into composable function

--- app/src/main/java/com/bayu/instagramhomepage/ui/reels/ReelsScreen.kt
+++ app/src/main/java/com/bayu/instagramhomepage/ui/reels/ReelsScreen.kt
@@ -35,214 +35,270 @@ import com.bayu.instagramhomepage.ui.utils.VideoPlayer
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.VerticalPager
 
-@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun ReelsScreen(
     reels: List<Reel> = Data.dummyDataReels,
 ) {
+    Reels(items = reels)
+}
 
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun Reels(
+    items: List<Reel>
+) {
     VerticalPager(
-        count = reels.size,
+        count = items.size,
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black),
     ) { index ->
-        val reel = reels[index]
+        val reel = items[index]
 
-        Box {
-            VideoPlayer(uri = reel.getUriVideo())
-            Surface(
-                color = Color.Transparent,
-                contentColor = Color.White
+        Reel(reel)
+    }
+}
+
+@Composable
+private fun Reel(
+    item: Reel
+) {
+    Box {
+        VideoPlayer(uri = item.getUriVideo())
+        Surface(
+            color = Color.Transparent,
+            contentColor = Color.White
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Bottom,
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Bottom,
-                ) {
-                    var isShowMoreDescription by remember {
-                        mutableStateOf(false)
-                    }
-
-                    Column(
-                        modifier = Modifier
-                            .padding(start = 12.dp, bottom = 12.dp, end = 32.dp, top = 24.dp)
-                            .weight(1F)
-                            .animateContentSize(
-                                animationSpec = tween(400)
-                            ),
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Surface(
-                                shape = CircleShape,
-                            ) {
-                                Image(
-                                    painter = rememberImagePainter(data = reel.user.profile),
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .size(30.dp),
-                                    contentScale = ContentScale.Crop,
-                                )
-                            }
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(
-                                text = reel.user.username,
-                                style = MaterialTheme.typography.subtitle2.copy(color = Color.White)
-                            )
-                            Spacer(modifier = Modifier.width(10.dp))
-                            Surface(
-                                shape = MaterialTheme.shapes.medium,
-                                color = Color.Transparent,
-                                contentColor = Color.White,
-                                border = BorderStroke(1.dp, Color.White),
-                            ) {
-                                Text(
-                                    text = if (reel.user.follow) "Unfollow" else "Follow",
-                                    modifier = Modifier
-                                        .padding(2.dp)
-                                        .padding(horizontal = 6.dp),
-                                    style = MaterialTheme.typography.body2,
-                                )
-                            }
-                        }
-                        Spacer(modifier = Modifier.heightIn(16.dp))
-
-                        val description = buildAnnotatedString {
-                            withStyle(style = MaterialTheme.typography.subtitle1.toSpanStyle()) {
-                                append(reel.description)
-                            }
-                            append("  ")
-                            withStyle(
-                                style = MaterialTheme.typography.body2.copy(
-                                    color = Color.White.copy(
-                                        alpha = .6F
-                                    )
-                                ).toSpanStyle()
-                            ) {
-                                val tags = reel.tags.joinToString(" ")
-                                append(tags)
-                            }
-                        }
-
-                        Text(
-                            text = description,
-                            modifier = Modifier
-                                .clickable(
-                                    interactionSource = remember { MutableInteractionSource() },
-                                    indication = null,
-                                ) {
-                                    isShowMoreDescription = !isShowMoreDescription
-                                },
-                            maxLines = if (isShowMoreDescription) Int.MAX_VALUE else 1,
-                            overflow = if (isShowMoreDescription) TextOverflow.Clip else TextOverflow.Ellipsis
-                        )
-
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Icon(
-                                imageVector = Icons.Rounded.GraphicEq,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(16.dp)
-                            )
-                            Spacer(modifier = Modifier.width(4.dp))
-                            Text(
-                                text = reel.music.name,
-                                style = MaterialTheme.typography.subtitle1.copy(fontSize = 14.sp)
-                            )
-                            Spacer(modifier = Modifier.width(3.dp))
-                            Surface(
-                                modifier = Modifier
-                                    .size(2.dp),
-                                shape = CircleShape,
-                                color = Color.White,
-                            ) { }
-                            Spacer(modifier = Modifier.width(3.dp))
-                            Text(
-                                text = if (reel.music.isOriginalAudio) "Original Audio" else reel.music.author,
-                                style = MaterialTheme.typography.subtitle1
-                            )
-                        }
-                    }
-                    Column(
-                        modifier = Modifier
-                            .padding(top = 24.dp, end = 12.dp, bottom = 12.dp)
-                            .fillMaxHeight(),
-                        verticalArrangement = Arrangement.SpaceBetween,
-                        horizontalAlignment = CenterHorizontally,
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.PhotoCamera,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(32.dp),
-                        )
-                        Column(
-                            horizontalAlignment = CenterHorizontally,
-                        ) {
-                            Column {
-                                Icon(
-                                    imageVector = Icons.Outlined.FavoriteBorder,
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .size(32.dp),
-                                )
-                                Spacer(modifier = Modifier.height(6.dp))
-                                Text(
-                                    text = reel.likes,
-                                    modifier = Modifier.align(CenterHorizontally)
-                                )
-                            }
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Column {
-                                Icon(
-                                    imageVector = Icons.Outlined.ModeComment,
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .size(32.dp),
-                                )
-                                Spacer(modifier = Modifier.height(6.dp))
-                                Text(
-                                    text = reel.comment,
-                                    modifier = Modifier.align(CenterHorizontally)
-                                )
-                            }
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Icon(
-                                imageVector = Icons.Outlined.Send,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(32.dp),
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Icon(
-                                imageVector = Icons.Outlined.MoreVert,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(32.dp),
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Surface(
-                                modifier = Modifier,
-                                shape = RoundedCornerShape(8.dp),
-                                border = BorderStroke(2.dp, Color.White),
-                            ) {
-                                Image(
-                                    painter = rememberImagePainter(data = reel.music.profile),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(35.dp),
-                                    contentScale = ContentScale.Crop
-                                )
-                            }
-                        }
-                    }
+                var isShowMoreDescription by remember {
+                    mutableStateOf(false)
                 }
+
+                ReelInfo(
+                    item = item,
+                    isShowMoreDescription = isShowMoreDescription,
+                    onIsShowMoreDescriptionChange = {
+                        isShowMoreDescription = !isShowMoreDescription
+                    }
+                )
+                ReelActions(item)
             }
+        }
+    }
+}
+
+@Composable
+fun ReelActions(item: Reel) {
+    Column(
+        modifier = Modifier
+            .padding(top = 24.dp, end = 12.dp, bottom = 12.dp)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = CenterHorizontally,
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.PhotoCamera,
+            contentDescription = null,
+            modifier = Modifier
+                .size(32.dp),
+        )
+        Column(
+            horizontalAlignment = CenterHorizontally,
+        ) {
+            Column {
+                Icon(
+                    imageVector = Icons.Outlined.FavoriteBorder,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(32.dp),
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Text(
+                    text = item.likes,
+                    modifier = Modifier.align(CenterHorizontally)
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            Column {
+                Icon(
+                    imageVector = Icons.Outlined.ModeComment,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(32.dp),
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Text(
+                    text = item.comment,
+                    modifier = Modifier.align(CenterHorizontally)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Icon(
+                imageVector = Icons.Outlined.Send,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(32.dp),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Icon(
+                imageVector = Icons.Outlined.MoreVert,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(32.dp),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Surface(
+                modifier = Modifier,
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(2.dp, Color.White),
+            ) {
+                Image(
+                    painter = rememberImagePainter(data = item.music.profile),
+                    contentDescription = null,
+                    modifier = Modifier.size(35.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RowScope.ReelInfo(
+    item: Reel,
+    isShowMoreDescription: Boolean,
+    onIsShowMoreDescriptionChange: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .padding(start = 12.dp, bottom = 12.dp, end = 32.dp, top = 24.dp)
+            .weight(1F)
+            .animateContentSize(
+                animationSpec = tween(400)
+            ),
+    ) {
+        ReelInfoUser(
+            item = item
+        )
+        Spacer(modifier = Modifier.heightIn(16.dp))
+        ReelInfoDescription(
+            item = item,
+            isShowMoreDescription = isShowMoreDescription,
+            onIsShowMoreDescriptionChange = onIsShowMoreDescriptionChange,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        ReelInfoMusic(item)
+    }
+}
+
+@Composable
+fun ReelInfoMusic(item: Reel) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.GraphicEq,
+            contentDescription = null,
+            modifier = Modifier
+                .size(16.dp)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = item.music.name,
+            style = MaterialTheme.typography.subtitle1.copy(fontSize = 14.sp)
+        )
+        Spacer(modifier = Modifier.width(3.dp))
+        Surface(
+            modifier = Modifier
+                .size(2.dp),
+            shape = CircleShape,
+            color = Color.White,
+        ) { }
+        Spacer(modifier = Modifier.width(3.dp))
+        Text(
+            text = if (item.music.isOriginalAudio) "Original Audio" else item.music.author,
+            style = MaterialTheme.typography.subtitle1
+        )
+    }
+}
+
+@Composable
+fun ReelInfoDescription(
+    item: Reel,
+    isShowMoreDescription: Boolean,
+    onIsShowMoreDescriptionChange: () -> Unit,
+) {
+    val description = buildAnnotatedString {
+        withStyle(style = MaterialTheme.typography.subtitle1.toSpanStyle()) {
+            append(item.description)
+        }
+        append("  ")
+        withStyle(
+            style = MaterialTheme.typography.body2.copy(
+                color = Color.White.copy(
+                    alpha = .6F
+                )
+            ).toSpanStyle()
+        ) {
+            val tags = item.tags.joinToString(" ")
+            append(tags)
+        }
+    }
+
+    Text(
+        text = description,
+        modifier = Modifier
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+            ) {
+                onIsShowMoreDescriptionChange.invoke()
+            },
+        maxLines = if (isShowMoreDescription) Int.MAX_VALUE else 1,
+        overflow = if (isShowMoreDescription) TextOverflow.Clip else TextOverflow.Ellipsis
+    )
+}
+
+@Composable
+fun ReelInfoUser(item: Reel) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Surface(
+            shape = CircleShape,
+        ) {
+            Image(
+                painter = rememberImagePainter(data = item.user.profile),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(30.dp),
+                contentScale = ContentScale.Crop,
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = item.user.username,
+            style = MaterialTheme.typography.subtitle2.copy(color = Color.White)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = Color.Transparent,
+            contentColor = Color.White,
+            border = BorderStroke(1.dp, Color.White),
+        ) {
+            Text(
+                text = if (item.user.follow) "Unfollow" else "Follow",
+                modifier = Modifier
+                    .padding(2.dp)
+                    .padding(horizontal = 6.dp),
+                style = MaterialTheme.typography.body2,
+            )
         }
     }
 }
