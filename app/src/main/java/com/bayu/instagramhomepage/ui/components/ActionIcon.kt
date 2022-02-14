017c61322e98721b2e29a59353fa8d09fed26d85
bayu07750 <bayu07750@gmail.com>
2022-02-14 09:02:01 +0700

    Add modifier param on ActionIcon

--- app/src/main/java/com/bayu/instagramhomepage/ui/components/ActionIcon.kt
+++ app/src/main/java/com/bayu/instagramhomepage/ui/components/ActionIcon.kt
@@ -2,8 +2,6 @@ package com.bayu.instagramhomepage.ui.components
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ChatBubbleOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -12,11 +10,13 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ActionIcon(
     imageVector: ImageVector,
-    contentDescription: String? = null
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
 ) {
     Icon(
         imageVector = imageVector,
         contentDescription = contentDescription,
-        modifier = Modifier.size(28.dp)
+        modifier = modifier
+            .size(28.dp),
     )
 }
