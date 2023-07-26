4d3d401ca69b6ad03f509cf1841661d5f6a223e5
bayu07750 <bayu07750@gmail.com>
2023-07-26 08:52:32 +0700

    extract to player composable

--- app/src/main/java/com/bayu/instagramhomepage/ui/reels/Player.kt
+++ app/src/main/java/com/bayu/instagramhomepage/ui/reels/Player.kt
@@ -31,58 +31,9 @@ fun Player(
     uri: Uri,
     pagerState: PagerState,
 ) {
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
-
-    val scope = rememberCoroutineScope()
-
-    var exoPlayer: ExoPlayer? by remember { mutableStateOf(null) }
-
     val mediaItem: MediaItem by remember(uri) { mutableStateOf(MediaItem.fromUri(uri)) }
-    var currentPlaybackPosition by remember { mutableStateOf(0L) }
-    var currentMediaItemIndex by remember { mutableStateOf(0) }
-    var playWhenReady by remember { mutableStateOf(false) }
 
-    fun initializePlayer() {
-        scope.launch {
-            exoPlayer = ExoPlayer.Builder(context)
-                .build()
-                .also { player ->
-                    player.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
-                    player.repeatMode = Player.REPEAT_MODE_ONE
-                    player.setMediaItems(listOf(mediaItem), currentMediaItemIndex, currentPlaybackPosition)
-                    player.playWhenReady = playWhenReady
-                    player.prepare()
-                }
-        }
-    }
-
-    fun releasePlayer() {
-        exoPlayer?.let { player ->
-            currentPlaybackPosition = player.currentPosition
-            currentMediaItemIndex = player.currentMediaItemIndex
-            playWhenReady = player.playWhenReady
-            player.release()
-        }
-        exoPlayer = null
-    }
-
-    DisposableEffect(key1 = lifecycleOwner, uri, effect = {
-        val observer = LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_START -> initializePlayer()
-                Lifecycle.Event.ON_STOP -> releasePlayer()
-                else -> {}
-            }
-        }
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    })
-
-
-    exoPlayer?.let {
+    Player(mediaItem = mediaItem) { exoPlayer, releaseFunc ->
         LaunchedEffect(key1 = pagerState, block = {
             combine(
                 flow = snapshotFlow { pagerState.currentPageOffsetFraction },
@@ -90,11 +41,10 @@ fun Player(
             ) { currentPageOffsetFraction, currentPage ->
                 currentPageOffsetFraction to currentPage
             }.filter { it.first == 0.0f }.collect { (_, currentPage) ->
-                exoPlayer ?: return@collect
                 if (currentPage == index) {
-                    exoPlayer!!.play() // play video hanya jika item seluruh tampil; atau snap 0.0 selain itu pause
+                    exoPlayer.play() // play video hanya jika item seluruh tampil; atau snap 0.0 selain itu pause
                 } else {
-                    exoPlayer!!.pause()
+                    exoPlayer.pause()
                 }
             }
         })
@@ -114,11 +64,72 @@ fun Player(
                 }
             },
             onRelease = {
-                releasePlayer()
+                releaseFunc.invoke()
             },
             onReset = {
                 // ignore
             }
         )
+    }
+}
+
+@SuppressLint("UnsafeOptInUsageError")
+@Composable
+fun Player(
+    mediaItem: MediaItem,
+    content: @Composable (exoPlayer: ExoPlayer, release: () -> Unit) -> Unit
+) {
+    val mediaItemState by rememberUpdatedState(newValue = mediaItem)
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val scope = rememberCoroutineScope()
+
+    var exoPlayer: ExoPlayer? by remember { mutableStateOf(null) }
+    var currentPlaybackPosition by remember { mutableStateOf(0L) }
+    var currentMediaItemIndex by remember { mutableStateOf(0) }
+    var playWhenReady by remember { mutableStateOf(false) }
+
+    fun initializePlayer() {
+        scope.launch {
+            exoPlayer = ExoPlayer.Builder(context)
+                .build()
+                .also { player ->
+                    player.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+                    player.repeatMode = Player.REPEAT_MODE_ONE
+                    player.setMediaItems(listOf(mediaItemState), currentMediaItemIndex, currentPlaybackPosition)
+                    player.playWhenReady = playWhenReady
+                    player.prepare()
+                }
+        }
+    }
+
+    fun releasePlayer() {
+        exoPlayer?.let { player ->
+            currentPlaybackPosition = player.currentPosition
+            currentMediaItemIndex = player.currentMediaItemIndex
+            playWhenReady = player.playWhenReady
+            player.release()
+        }
+        exoPlayer = null
+    }
+
+    DisposableEffect(key1 = lifecycleOwner, effect = {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_START -> initializePlayer()
+                Lifecycle.Event.ON_STOP -> releasePlayer()
+                else -> {}
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    })
+
+    exoPlayer?.let {
+        content.invoke(it, ::releasePlayer)
     }
 }
